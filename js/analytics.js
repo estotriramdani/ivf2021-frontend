f7d8ca6031ab86f0de175b7872276ff16413cab3
Esto Triramdani N <estolagi@gmail.com>
2021-10-13 11:33:11 +0700

    add analytics

---
+++ js/analytics.js
@@ -0,0 +1,72 @@
+let latitude = '';
+let longtitude = '';
+async function sendData() {
+  try {
+    const response = await fetch('https://httpbin.org/ip');
+    const page_name = document.title;
+    const current_position = getLocation().then((res) => console.log(res));
+    console.log(current_position);
+    let device = '';
+    if (window.innerWidth > 992) {
+      device = 'desktop';
+    } else if (window.innerWidth > 768) {
+      device = 'tablet';
+    } else {
+      device = 'mobile';
+    }
+
+    const responseJson = await response.json();
+
+    var formdata = new FormData();
+    formdata.append('page_name', page_name);
+    formdata.append('user_ip', responseJson.origin);
+    formdata.append('latitude', latitude);
+    formdata.append('longtitude', longtitude);
+    formdata.append('device', device);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    };
+
+    fetch('http://localhost/ivf-analytics/api/save-data.php', requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+sendData();
+
+async function getLocation() {
+  if (navigator.geolocation) {
+    return navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+  }
+}
+
+function showPosition(position) {
+  latitude = position.coords.latitude;
+  longtitude = position.coords.longitude;
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.log('User denied the request for Geolocation.');
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log('Location information is unavailable.');
+      break;
+    case error.TIMEOUT:
+      console.log('The request to get user location timed out.');
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log('An unknown error occurred.');
+      break;
+  }
+}
