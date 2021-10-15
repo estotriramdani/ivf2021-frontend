bd8bb1c6a8b4001827f4500e4874ae5da2ee35a7
Ayu Aulia <ayu.auliafr@gmail.com>
2021-10-15 11:33:27 +0700

    update logo linkaja

--- js/components/home/sponsor.js
+++ js/components/home/sponsor.js
@@ -1,32 +1,21 @@
 const sponsorOne = `
 <a href="company-detail.html">
   <img
-    src="logo/bi.png"
-    alt="logo bank indonesia"
+    src="logo/linkaja.png"
+    alt="logo LinkAja"
     class="img-fluid"
   />
 </a>
 `;
 
-const sponsorTwo = `
-<a href="company-detail.html">
-  <img
-    src="logo/pegadaian-logo.png"
-    alt="logo pegadaian"
-    class="img-fluid"
-  />
-</a>
-`;
+
 
 const sponsorHTML = `
 <div class="row justify-content-center px-4">
   <div class="col-6 col-md-3 col-lg-4">
     ${sponsorOne}
   </div>
-  <div class="col-6 col-md-3 col-lg-4">
-    ${sponsorTwo}
-  </div>
 </div>
 `;
 
-// document.querySelector('.sponsor-image').innerHTML = sponsorHTML;
+document.querySelector('.sponsor-image').innerHTML = sponsorHTML;
