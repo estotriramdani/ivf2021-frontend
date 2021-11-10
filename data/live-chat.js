f607d74689854a8b83f642f8d4f9593f11aa2ab7
Ayu Aulia <ayu.auliafr@gmail.com>
2021-11-10 14:06:09 +0700

    Update live-chat.js

--- data/live-chat.js
+++ data/live-chat.js
@@ -1,16 +1,16 @@
 const liveChatCommpanies = [
   {
     id: 1,
+    name: 'Coca-Cola Europacific Partners Indonesia (CCEP Indonesia)',
+    logo: 'coca-cola-europacific-partners-indonesia.png',
+    link: 'company-detail/coca-cola-europacific-partners-indonesia.html',
+    width: '200',
+  },{
+    id: 2,
     name: 'Niagahoster',
     logo: 'niagahoster.jpeg',
     link: 'company-detail/niagahoster.html',
     width: '200',
   },
-  {
-    id: 2,
-    name: 'Coca-Cola Europacific Partners Indonesia (CCEP Indonesia)',
-    logo: 'coca-cola-europacific-partners-indonesia.png',
-    link: 'company-detail/coca-cola-europacific-partners-indonesia.html',
-    width: '200',
-  },
+  
 ];
