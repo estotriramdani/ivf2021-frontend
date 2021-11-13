9fa45b15397f5d93b9fe5b86b44dd12ef1abb01f
Ayu Aulia <ayu.auliafr@gmail.com>
2021-11-13 08:53:09 +0700

    livechat

--- data/live-chat.js
+++ data/live-chat.js
@@ -5,11 +5,19 @@ const liveChatCommpanies = [
     logo: 'coca-cola-europacific-partners-indonesia.png',
     link: 'company-detail/coca-cola-europacific-partners-indonesia.html',
     width: '200',
-  },{
+  },
+  {
     id: 2,
     name: 'Niagahoster',
     logo: 'niagahoster.jpeg',
     link: 'company-detail/niagahoster.html',
+    width: '200',
+  },
+  {
+    id: 3,
+    name: 'Bina Nusantara Group',
+    logo: 'bina-nusantara-group.png',
+    link: 'company-detail/bina-nusantara-group.html',
     width: '200',
   },
   
